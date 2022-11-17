031dbdbdd817d70defa18128ac557edac1207e3b
wowblast <jfrt142012@hotmail.com>
2022-11-17 11:51:36 -0400

    Delete comments on homework

--- homework17-11-2022/virtualboxcli.py
+++ homework17-11-2022/virtualboxcli.py
@@ -1,12 +1,6 @@
 import virtualbox;
-
-import sys
+import sys 
  
-# total arguments
-n = len(sys.argv)
-print("Total arguments passed:", n)
- 
-# Arguments passed
 print("\nName of Python script:", sys.argv[0])
 vbox = virtualbox.VirtualBox()
 
@@ -26,25 +20,4 @@ elif sys.argv[1] == "standUp":
   print("session status: " +str(session.state))
   print("machine status: " +str(machine.state))
 
-
 print("end");
-"""session = virtualbox.Session()
-session = virtualbox.Session()
-machine = vbox.find_machine("UBUNTU2004_default_1668471086686_97993")
-#progress = machine.launch_vm_process(session, "gui", "")
-#>>> # For virtualbox API 6_1 and above (VirtualBox 6.1.2+), use the following:
-progress = machine.launch_vm_process(session, "gui", [])
-progress.wait_for_completion()
-
-filename = vbox.compose_machine_filename('addmachine','/test','','E:\virtualbox')
-
-m = vbox.create_machine(filename,"testCreate",['/test'],"Linux",'forceOverwrite=0')
-vbox.registerMachine(m)
-E:\virtualbox
-
-m = vbox.createMachine(_arg_settingsFile='',
-                                   _arg_name="testCreate",
-                                   _arg_groups=['/test'],
-                                   _arg_osTypeId="Linux",
-                                   _arg_flags='forceOverwrite=0')
-vbox.registerMachine(m)"""
